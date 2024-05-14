55c248e4de9a0c34f68be3ac82739857e17293a4
AndyChouCoding <andychouforwk@gmail.com>
2024-05-14 16:05:37 +0800

    fix

--- src/pages/gamemode/index.tsx
+++ src/pages/gamemode/index.tsx
@@ -13,9 +13,9 @@ const GameMode = () => {
   const [touchEndX, setTouchEndX] = useState(0);
 
   const images = [
-    { src: GetMode, alt: "快速捕獲", path: "/getMode", label: "快速捕獲" },
-    { src: PKMode, alt: "活動對戰", path: "/pkMode", label: "活動對戰" },
     { src: PkGetMode, alt: "對戰捕獲", path: "/pkgetMode", label: "對戰捕獲" },
+    { src: PKMode, alt: "活動對戰", path: "/pkMode", label: "活動對戰" },
+    { src: GetMode, alt: "快速捕獲", path: "/getMode", label: "快速捕獲" },
   ];
   const nextSlide = () => {
     setCurrentMode(
@@ -32,50 +32,12 @@ const GameMode = () => {
   };
   const displayIndices = getDisplayIndices(currentMode, images.length);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-
-  const handleTouchStart = (e: any) => {
-    setTouchStartX(e.touches[0].clientX);
-  };
-  const handleTouchMove = (e: any) => {
-    setTouchEndX(e.touches[0].clientX);
-  };
-
-  const handleTouchEnd = () => {
-    if (touchStartX - touchEndX > 50) {
-      nextSlide();
-    }
-
-    if (touchStartX - touchEndX < -50) {
-      prevSlide();
-    }
-  };
+  
 
   return (
     <>
       <Layout className="mode_page_bg relative">
-        <div className=" absolute left-1 right-1 top-[40vh] flex justify-center items-center "
-             onTouchStart={handleTouchStart}
-             onTouchMove={handleTouchMove}
-             onTouchEnd={handleTouchEnd}
-        >
-          {!isMobile && (
-            <button
-              onClick={prevSlide}
-              className="w-[30px] h-[30px] rounded-full  m-1 bg-gray-300"
-            >
-              {"<"}
-            </button>
-          )}
+        <div className=" absolute left-1 right-1 top-[40vh] flex justify-center items-center ">
           <div className="flex justify-center text-center ">
             {displayIndices.map((index) => (
               <div
@@ -92,14 +54,6 @@ const GameMode = () => {
               </div>
             ))}
           </div>
-          {!isMobile && (
-            <button
-              onClick={nextSlide}
-              className="w-[30px] h-[30px] rounded-full  m-1 bg-gray-300"
-            >
-              {">"}
-            </button>
-          )}
         </div>
       </Layout>
     </>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,7 +8,7 @@ const Home = () => {
     <>
       <Layout className="home_page_bg">
         <div className=" relative">
-          <div onClick={()=>{go("/login")}} className="h-[88vh]"></div>
+          <div onClick={()=>{go("/login")}} className="h-[60vh] md:h-[88vh]"></div>
           <p className=" absolute top-[60vh] p-2 text-center bg-slate-200 opacity-70 w-[100%]">
             請點擊任意處開始
           </p>
