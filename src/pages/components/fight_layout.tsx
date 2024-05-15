99edf0e60462584a828f0b4549c702901b3a8d2d
AndyChouCoding <andychouforwk@gmail.com>
2024-05-15 13:47:10 +0800

    fix

--- src/pages/components/fight_layout.tsx
+++ src/pages/components/fight_layout.tsx
@@ -8,7 +8,7 @@ interface FightBtnProps {
 
 const FightBtn:React.FC<FightBtnProps> = ({ count, setCount}) => {
     return<>
-        <div className="flex justify-around items-center text-center bg-slate-200 p-2 opacity-80 absolute top-[188px] w-[100%] ">
+        <div className="flex justify-around items-center text-center bg-slate-200 p-2 opacity-80 w-[100%] ">
             <button onClick={()=> setCount(count + 3)} className="w-[50px] h-[50px] rounded-full border-4 border-solid border-white bg-blue-400">Left</button>
             <div className="flex p-2 text-black absolute "><span className="mx-2 ">{count}</span><p>%</p></div>
             <button onClick={()=> setCount(count + 3)} className="w-[50px] h-[50px] rounded-full border-4 border-solid border-white bg-orange-400">Right</button>
@@ -28,10 +28,11 @@ const FightLayout = () => {
             <div className="w-[30px] h-[30px] mx-[1px] border-2 border-blue-500 bg-white rounded-full absolute top-2 z-[99]">3</div>
             <div className="w-[30px] h-[30px] mx-[1px] border-2 border-blue-500 bg-white rounded-full absolute top-2 left-6 z-[99]">4</div>
         </div>
-        <div className="h-[254px] fightting_bg_my relative">
+        <div className="h-[34vh] sm:h-[38vh] lg:h-[39vh] fightting_bg_my ">
             <div></div>
-            <FightBtn count={count} setCount={setCount}/>
+            
         </div>
+        <FightBtn count={count} setCount={setCount}/>
       </div>
     </>
   );
